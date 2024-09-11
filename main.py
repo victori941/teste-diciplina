30eb77356d1bcdb5a92f0ca8fbfeff91f7784b7e
liniit <victoriaaline10032002@gmail.com>
2024-09-11 17:55:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 
 # 127.0.0.1:8001/
@@ -10,4 +10,4 @@ async def root():
 # 127.0.0.1:8001/teste1
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": "Deu certo"}
+    return {"teste": True "num_aleatorio": random.randint(1, 100)}
