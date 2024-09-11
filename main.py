ab0b4fbde01b44b347e9c4b242f5d1c39c477978
liniit <victoriaaline10032002@gmail.com>
2024-09-11 20:00:11 -0300

    Outro teste

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-@app.get("/teste")
+@app.get("/teste1")
 async def funcaoteste():
     return {"teste": "Deu certo"}
